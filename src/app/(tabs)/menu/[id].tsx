aab1c1d5f38003aa492070d69a30cf8462992060
birbalkr <birbalkr1435@gmail.com>
2026-03-24 04:07:03 +0530

    Product details Screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,14 +1,85 @@
+import Button from '@/components/Button';
+import { defaultImage } from '@/components/ProductListItem';
+import products from '@assets/data/products';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+
+const sizes = ['S', 'M', 'L', 'XL'];
 
 const ProductDetailsScreen = () => {
-  const {id} = useLocalSearchParams();
+  const { id } = useLocalSearchParams();
+
+  const [selectedSize, setSelectedSize] = useState('M');
+
+  const product = products.find((p) => p.id.toString() === id);
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
   return (
-    <View>
-      <Stack.Screen options={{headerTitle: 'Details'}} />
-      <Text>product : {id}  </Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ headerTitle: product.name }} />
+      <Image source={{ uri: product.image || defaultImage }} style={styles.image} />
+
+      <Text>Select size</Text>
+      <View style={styles.sizes}>
+        {sizes.map(size => (
+          <Pressable 
+          onPress={()=>{setSelectedSize(size)}}
+          style={[styles.size,  { 
+          backgroundColor: selectedSize === size ? 'gainsboro' : 'white'
+        }]} key={size}>
+            <Text style={[styles.sizeText, {color: selectedSize === size ? 'black' : 'gray'}]}>{size}</Text>
+          </Pressable>
+        ))}
+
+      </View>
+
+
+      <Text style={styles.price}>$ {product.price}</Text>
+
+      <Button text='Add to cart' onPress={() => {}} />
     </View>
   )
 }
 export default ProductDetailsScreen
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+
+  },
+  sizes: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: 'gainsboro',
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  sizeText: {
+    fontSize:20,
+    fontFamily:'bold',
+  },
+  price: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop:'auto',
+  },
+
+
+
+})
